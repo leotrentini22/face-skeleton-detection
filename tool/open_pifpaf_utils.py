79347608e858174718f97e03393f935c544ebaad
leotrentini22 <trentini.leonardo00@gmail.com>
2023-04-30 17:26:38 +0200

    typos

--- tool/open_pifpaf_utils.py
+++ tool/open_pifpaf_utils.py
@@ -4,10 +4,6 @@ import os
 import openpifpaf
 from PIL import Image
 import json
-from dataset import * 
-from utils import *
-
-import subprocess
 
 def calculate_skeleton(image_path, output_dir):
     checkpoint = 'shufflenetv2k30-wholebody'

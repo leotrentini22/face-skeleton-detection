36779d5e31c1e89516c8a72160e4b1f4987e0d9b
leotrentini22 <trentini.leonardo00@gmail.com>
2023-04-30 12:17:19 +0200

    bugs

--- openpifpafprova.py
+++ openpifpafprova.py
@@ -9,12 +9,10 @@ from utils import *
 
 import subprocess
 
-print("entra")
-
+print("entra qui")
 
 def calculate_skeleton(image_path, output_dir):
     checkpoint = 'shufflenetv2k30-wholebody'
-    model = openpifpaf.network.factory(checkpoint)
 
     # Create the output directory if it doesn't exist
     os.makedirs(output_dir, exist_ok=True)
@@ -23,7 +21,7 @@ def calculate_skeleton(image_path, output_dir):
     json_output = os.path.join(output_dir, os.path.splitext(os.path.basename(image_path))[0] + '.json')
 
     # Call the predict_image() function with the appropriate arguments
-    openpifpaf.predict.predict_image(image_path, model, json_output=json_output)
+    openpifpaf.predict.predict_image(image_path, checkpoint=checkpoint, image_output=False, json_output=json_output)
 
 actual_image_path = '/work/vita/datasets/Aff-Wild2/cropped_aligned/430/00262.jpg'
 skeleton_output_dir = '/home/trentini/face-skeleton-detection'

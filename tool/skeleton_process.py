13ee97d41cb7d07683f74eb6af6cc6da2f2d3788
leotrentini22 <trentini.leonardo00@gmail.com>
2023-05-09 12:28:28 +0200

    test skeletons

--- tool/skeleton_process.py
+++ tool/skeleton_process.py
@@ -49,18 +49,18 @@ test_list = os.listdir(os.path.join(label_root, test_path))
 
 # Validation
 
-for val_txt in val_list:
-    with open(os.path.join(os.path.join(label_root, val_path), val_txt), 'r') as f:
-        lines = f.readlines()
-    lines = lines[1:]
-    for j, line in enumerate(lines):
-        line = line.rstrip('\n').split(',')
-        line = np.array(line).astype(np.int32)
-        if -1 in line:
-            continue
-        actual_img_path = os.path.join(os.path.join(img_path_vita,os.path.basename(os.path.normpath(val_txt.split('.')[0]))), str(j+1).zfill(5)+'.jpg')
-        skeleton_output_dir = os.path.join(skeleton_path,val_path)
-        calculate_skeleton(actual_img_path, skeleton_output_dir)
+# for val_txt in val_list:
+#     with open(os.path.join(os.path.join(label_root, val_path), val_txt), 'r') as f:
+#         lines = f.readlines()
+#     lines = lines[1:]
+#     for j, line in enumerate(lines):
+#         line = line.rstrip('\n').split(',')
+#         line = np.array(line).astype(np.int32)
+#         if -1 in line:
+#             continue
+#         actual_img_path = os.path.join(os.path.join(img_path_vita,os.path.basename(os.path.normpath(val_txt.split('.')[0]))), str(j+1).zfill(5)+'.jpg')
+#         skeleton_output_dir = os.path.join(skeleton_path,val_path)
+#         calculate_skeleton(actual_img_path, skeleton_output_dir)
 
 
 # Test
